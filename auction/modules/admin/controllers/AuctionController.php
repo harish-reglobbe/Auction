2e05f0ffce96f3878b72b5c5ad83891c8056692d
harish-reglobbe <harish.r@reglobbe.co>
2015-08-24 19:11:48 +0530

    Updated Project !! with admin module

---
+++ auction/modules/admin/controllers/AuctionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace auction\modules\admin\controllers;
+
+use Yii;
+use auction\models\Auctions;
+use auction\models\forms\SearchAuction;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * AuctionController implements the CRUD actions for Auctions model.
+ */
+class AuctionController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Auctions models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new SearchAuction();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Auctions model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+
+    /**
+     * Deletes an existing Auctions model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Auctions model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Auctions the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Auctions::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
